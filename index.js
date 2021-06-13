73fe59d27b4ef73250345c798209af9be2252735
Viktoria <doduchenko29@gmail.com>
2021-06-13 15:10:40 +0300

    add

---
+++ index.js
@@ -0,0 +1,51 @@
+const listElem = document.querySelector('.list');
+
+const tasks = [
+  { text: 'Buy milk', done: false, id: Math.random() },
+  { text: 'Pick up Tom from airport', done: false, id: Math.random() },
+  { text: 'Visit party', done: false, id: Math.random() },
+  { text: 'Visit doctor', done: true, id: Math.random() },
+  { text: 'Buy meat', done: true, id: Math.random() },
+];
+
+const inputElem = document.querySelector('.task-input');
+const buttonElem = document.querySelector('.create-task-btn');
+
+const createTask = () => {
+  if (inputElem.value == '') {
+    return;
+  }
+
+  tasks.push({ text: inputElem.value, done: false, id: Math.random() });
+};
+
+buttonElem.addEventListener('click', createTask);
+
+const renderTasks = (tasksList) => {
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+  listElem.innerHTML = '';
+  listElem.append(...tasksElems);
+};
+
+renderTasks(tasks);
+
+const complete = (event) => {
+  
+};
+
+renderTasks(tasks);
